216e840df76e3451289e4c44da61e42297d2dfcc
AlziraXerinda <alziraxerinda0@gmail.com>
2024-03-08 09:33:40 +0200

    [EMISWEB-111] enhancement: improved getRelative time function

--- src/utils/commons/getRelativeTime.ts
+++ src/utils/commons/getRelativeTime.ts
@@ -1,3 +1,4 @@
+
 export function getRelativeTime(from: Date): string {
     const now = new Date();
     const difference = now.getTime() - from.getTime();
@@ -9,19 +10,20 @@ export function getRelativeTime(from: Date): string {
     const months = Math.floor(days / 30);
     const years = Math.floor(days / 365);
 
-    if (years > 0) {
-        return `${years} year${years > 1 ? 's' : ''} ago`;
-    } else if (months > 0) {
-        return `${months} month${months > 1 ? 's' : ''} ago`;
-    } else if (weeks > 0) {
-        return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
-    } else if (days > 0) {
-        return `${days} day${days > 1 ? 's' : ''} ago`;
-    } else if (hours > 0) {
-        return `${hours} hour${hours > 1 ? 's' : ''} ago`;
-    } else if (minutes > 0) {
-        return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
-    } else {
-        return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+    switch (true) {
+        case years > 0:
+            return `${years} year${years > 1 ? 's' : ''} ago`;
+        case months > 0:
+            return `${months} month${months > 1 ? 's' : ''} ago`;
+        case weeks > 0:
+            return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
+        case days > 0:
+            return `${days} day${days > 1 ? 's' : ''} ago`;
+        case hours > 0:
+            return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+        case minutes > 0:
+            return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+        default:
+            return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
     }
 }
